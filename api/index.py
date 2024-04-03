0a2980d2e5ea66713597389fd9b0e9dc005cc150
ccjustin <ccjustin@umich.edu>
2024-04-02 20:34:11 -0400

    functions drafts in api/index.py:

--- api/index.py
+++ api/index.py
@@ -2,8 +2,21 @@ from flask import Flask
 from app import mnportal
 import app
 
+def get_team(team:str):
+    '''Return a list of team members.'''
+
+def get_projects(team:str):
+    '''Return a list of the team's projects.'''
+
+def get_officers():
+    '''Return all columns on all officers.'''
+
+def get_teams():
+    '''Return names of all the teams.'''
+
 @mnportal.route("/api/python")
 def hello_world():
+    '''Basic functionality test.'''
     connection = app.model.get_db()
     cur = connection.execute("SELECT * FROM users")
 
